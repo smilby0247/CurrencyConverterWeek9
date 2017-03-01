0942eb25492304e488fc05758f5eac72c56c9695
Shaun Milby <smilby0247@my.sctd.edu>
2017-02-28 21:17:30 -0500

    Buncha changes
    Here they are. Week 9 update. In this update I have both of the spinners
    working properly. The last step is to get the function working properly
    to convert, then the app will function for its purpose.

---
+++ MainActivity.cs
@@ -0,0 +1,77 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+using CurrencyConverter.Models;
+using System;
+
+namespace CurrencyConverter
+{
+    [Activity(Label = "CurrencyConverter", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        
+
+        public List<Converter> otherValues { get; set; }
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            // list of countries in relationships to the USD
+
+            otherValues = new List<Converter>();
+            otherValues.Add(new Converter {Country = "CAD", valueInUSD = 1.31 });
+            otherValues.Add(new Converter { Country = "GBP", valueInUSD = 0.92 });
+            otherValues.Add(new Converter { Country = "EUR", valueInUSD = 0.54 });
+
+            Spinner spinner1 = FindViewById<Spinner>(Resource.Id.Spinner1);
+            spinner1.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected); 
+
+            var adapter = ArrayAdapter.CreateFromResource(
+                this, Resource.Array.selectCurrency, Android.Resource.Layout.SimpleSpinnerDropDownItem);
+              spinner1.Adapter = adapter;
+
+            Spinner spinner2 = FindViewById<Spinner>(Resource.Id.Spinner2);
+            spinner2.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+
+            var adapter2 = ArrayAdapter.CreateFromResource(
+                this, Resource.Array.selectCurrency, Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            spinner2.Adapter = adapter;
+            
+
+        }
+
+        private void Spinner2_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            Spinner spinner2 = (Spinner)sender;
+
+            
+            throw new NotImplementedException();
+        }
+
+        private void spinner_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            Spinner spinner1 = (Spinner)sender;
+
+           
+            
+
+
+            
+
+           
+        }
+
+      
+        
+        }
+
+
+    }
+
+
+
+
---
+++ Models/Converter.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+
+
+namespace CurrencyConverter.Models
+{
+    public class Converter
+
+        // this establishes that the types exist. Now we can make our list
+        // of countries and appropriate their values and such.
+    {
+        public string Country { get; set; }
+        public double valueInUSD { get; set; }
+
+
+
+
+    }
+
+   
+}
